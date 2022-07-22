5bd13b2b6967aa4495ce47c77a96d3ee24868c7d
Suvanth Ramruthen <rmrsuv002@cs.uct.ac.za>
2022-07-23 01:01:41 +0200

    code updated Build

--- app/src/main/java/com/example/ecd_app/MainActivity.kt
+++ app/src/main/java/com/example/ecd_app/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.ecd_app
 
-import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.example.ecd_app.room.PostsViewModel
-import com.example.ecd_app.room.PostsViewModelFactory
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ecd_app.room.Post
+import com.example.ecd_app.room.PostsViewModel
+import com.example.ecd_app.room.PostsViewModelFactory
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -19,64 +26,64 @@ class MainActivity : AppCompatActivity() {
     private val wordViewModel: PostsViewModel by viewModels {
         PostsViewModelFactory((application as ECDApplication).repository)
     }
-    var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.text) as TextView
-        textView!!.text = "Setting text"
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = PostListAdapter(){
+
+            Toast.makeText(this@MainActivity,"hi from main", Toast.LENGTH_LONG).show()
+
+        }
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        //connection
+        wordViewModel.allPosts.observe(this) { words ->
+            // Update the cached copy of the words in the adapter.
+            words.let { adapter.submitList(it) }
+        }
+
+
         lifecycleScope.launch {
-            //textView!!.text = "Setting text"
-            //textView!!.text = getSQL()
-            var s = getSQL()
+            getSQL()
         }
     }
 
-    suspend fun getSQL(): String? {
+    suspend fun getSQL() {
         // Use a different CoroutineScope, etc
-        var result: String? = null
         CoroutineScope(Dispatchers.IO).launch {
             var SQLConnector = SQLConnector()
             SQLConnector.getConnection()
-            var queryResultSet = SQLConnector.executeMySQLQuery("SELECT * FROM wp_posts") //"SELECT post_content FROM wp_posts;"
+            var queryResultSet = SQLConnector.executeMySQLQuery("SELECT post_content FROM wp_posts where ID=85;")
 
             //saving to database
             System.out.println("Saving to db")
             val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
             val currentDate = sdf.format(Date())
-
-            var row = 1
-            while(queryResultSet!!.next()){
-                System.out.println("Row : "+ row)
-            queryResultSet!!.next()
-                val columnsNumber: Int = queryResultSet!!.metaData.columnCount
-                for(i in 1..columnsNumber){
-                    System.out.println("Column :"+i + " "+queryResultSet.getString(i))
+            if (queryResultSet != null) {
+                queryResultSet.next()
+                System.out.println(queryResultSet.getString(1))
+            }
+            val post =
+                queryResultSet?.let {
+                    Post(
+                        0,
+                        "Title",
+                        currentDate.toString(),
+                        it.getString(1),
+                        "meta"
+                    )
                 }
-                row++
-//                val post =
-//                    queryResultSet?.let {
-//                        Post(
-//                            0,
-//                            "Title",
-//                            currentDate.toString(),
-//                            it.getString(1),
-//                            "meta"
-//                        )
-//                    }
-//                System.out.println("Post created "+ post)
-//                result  = queryResultSet!!.getString(1)
-//                System.out.println(result)
-//                if (post != null) {
-//                    wordViewModel.insert(post)
-//                    System.out.println("Checking db" + wordViewModel.allPosts.value)
-//                }
+            System.out.println("Post created "+ post)
+            if (post != null) {
+                wordViewModel.insert(post)
+                System.out.println("Checking db" + wordViewModel.allPosts.value)
             }
             SQLConnector.closeConnection()
         }
-        return result
     }
 
 }
