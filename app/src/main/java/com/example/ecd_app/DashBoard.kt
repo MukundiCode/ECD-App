85555e0a0516be4b95ef001d24d7062144ea86e3
Suvanth <80575775+Suvanth@users.noreply.github.com>
2022-07-24 21:53:40 +0200

    basic flow and ui demo ready

--- app/src/main/java/com/example/ecd_app/DashBoard.kt
+++ app/src/main/java/com/example/ecd_app/DashBoard.kt
@@ -1,11 +1,29 @@
 package com.example.ecd_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.viewModels
+import androidx.cardview.widget.CardView
 
 class DashBoard : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash_board)
+        val cardContent : CardView = findViewById(R.id.cardContent)
+        cardContent.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+//        val btnContent : Button = findViewById(R.id.btnContent)
+//
+//        btnContent.setOnClickListener(){
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
