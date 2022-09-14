a94a8eb51d5835886c1cdab6c350af8b94f14780
suvanthramruthen <rsuvanth@gmail.com>
2022-09-14 10:14:28 +0200

    homePageTest added

---
+++ app/src/androidTest/java/com/example/ecd_app/LoginActivityTest.kt
@@ -0,0 +1,81 @@
+package com.example.ecd_app
+
+
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class LoginActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(LoginActivity::class.java)
+
+    @Test
+    fun loginActivityTest() {
+        val imageView = onView(
+            allOf(
+                withId(R.id.imageView),
+                withParent(withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))),
+                isDisplayed()
+            )
+        )
+        imageView.check(matches(isDisplayed()))
+
+        val button = onView(
+            allOf(
+                withId(R.id.btnLogin), withText("LOGIN"),
+                withParent(withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))),
+                isDisplayed()
+            )
+        )
+        button.check(matches(isDisplayed()))
+
+        val button2 = onView(
+            allOf(
+                withId(R.id.btnGuest), withText("CONTINUE AS GUEST"),
+                withParent(withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))),
+                isDisplayed()
+            )
+        )
+        button2.check(matches(isDisplayed()))
+
+        val textView = onView(
+            allOf(
+                withText("Enter your username:"),
+                withParent(withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Enter your username:")))
+
+        val textView2 = onView(
+            allOf(
+                withText("OR"),
+                withParent(withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("OR")))
+
+        val textView3 = onView(
+            allOf(
+                withText("OR"),
+                withParent(withParent(IsInstanceOf.instanceOf(android.view.ViewGroup::class.java))),
+                isDisplayed()
+            )
+        )
+        textView3.check(matches(withText("OR")))
+    }
+}
